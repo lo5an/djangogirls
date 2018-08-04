767546222fbd2012a7ba8e1c9408dd99eb342442
Logan <lo5an@lo5an.com>
2018-08-03 21:58:07 -0500

    initial code commit.

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone 
+
+
+class Post (models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_datee =  models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publshed_date = timezoe.now()
+        self.save()
+
+    def __str__(Self):
+        return self.title
